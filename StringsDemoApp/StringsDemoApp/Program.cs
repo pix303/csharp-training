633184ef11b9695867084fb1c3c0e7f2acf9037d
paolo.carraro <paolo.carraro@interlogica.it>
2022-01-15 23:27:19 +0100

    First test with strings

---
+++ StringsDemoApp/StringsDemoApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+// substring using index
+string data = "This is a test about strings";
+Console.WriteLine(data[5..]);
+Console.WriteLine(data[5..7]);
+Console.WriteLine(data[..8]);
+Console.WriteLine(data[data.IndexOf("test")..]);
+
+// string concatenation
+string[] datas = { "zero", "one", "two", "three", "four", "five" };
+
+// concat by + operator
+string buildByOperator = "";
+// string builders
+StringBuilder sb = new StringBuilder();
+StringBuilder sbp = new StringBuilder();
+
+for (int i = 0; i < datas.Length; i++) {
+    // operator += and +
+    buildByOperator += datas[i] + " ";
+    // append in line
+    sb.Append($"{datas[i]} - ");
+    // append new line
+    sbp.AppendLine(datas[i]);
+}
+
+Console.WriteLine(buildByOperator);
+Console.WriteLine(sb.ToString());
+Console.WriteLine(sbp.ToString());
+
+// concat static method of string
+Console.WriteLine( "Concat: " + String.Concat(datas));
+// join with separator static method string
+Console.WriteLine( "Join  : {0}", String.Join(" - ", datas));
